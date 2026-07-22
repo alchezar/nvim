15a379f995645ab4ae42c63dbcb68027b0e3476b
Ivan Kinder <kinder.wit@gmail.com>
2026-07-22 10:44:52 +0300

    feat(keys): add resume-last-search mapping

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -76,6 +76,7 @@ map('n', '<leader>fg',
   function() builtin.live_grep({ additional_args = function() return { '--fixed-strings' } end }) end,
   { desc = 'Live grep (literal)' })
 map('n', '<leader>fG', builtin.live_grep, { desc = 'Live grep' })
+map('n', '<leader>f.', builtin.resume, { desc = 'Resume last search' })
 map('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
 map('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
 map('n', '<leader>fk', builtin.keymaps, { desc = 'Search keymaps' })
